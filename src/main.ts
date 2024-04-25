1a594dfe7b6ca69e026b7f1bb900d17ea1b83704
Ulysseassoo <ulysse.asso-o-emane@supinternet.fr>
2024-04-26 01:51:37 +0200

    Added room state when changing rooms

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,23 @@ const generateRandomNumber = () => {
     return Math.floor(Math.random() * 100) + 1;
 }
 
+const initState = () => {
+    // const roomName = window.location.pathname;
+    // console.log("🚀 ~ initState ~ roomName:", roomName)
+    const waitingRoom = WA.state.loadVariable("waiting-room")
+    console.log("🚀 ~ initState ~ waitingRoom:", waitingRoom)
+    if (!waitingRoom) {
+        WA.state.saveVariable("waiting-room", {
+            actualRoom: "hopital-1"
+        });
+    }
+}
+
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
-
+    console.log('Player tags: ', WA.player.tags)
+    initState()
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
         const time = today.getHours() + ":" + today.getMinutes();
@@ -24,7 +36,11 @@ WA.onInit().then(() => {
     WA.room.area.onLeave('clock').subscribe(closePopup)
 
     WA.room.onEnterLayer("stairs_zone").subscribe(() => {
-        WA.nav.goToRoom(`https://play.workadventu.re/_/hopital-${generateRandomNumber()}/localhost:5173/map.tmj`)
+        const room = `hopital-${generateRandomNumber()}`
+        WA.state.saveVariable("waiting-room", {
+            actualRoom: room
+        });
+        WA.nav.goToRoom(`https://play.workadventu.re/_/${room}/localhost:5173/map.tmj`)
     })
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
@@ -36,18 +52,18 @@ WA.onInit().then(() => {
 
 
 
-    WA.room.area.onEnter('test-area').subscribe(() => {
-        currentPopup = WA.ui.openPopup("clockPopup", "Bienvenue dans la magie clinique", []);
-        console.log("Here welcome");
-    })
+WA.room.area.onEnter('test-area').subscribe(() => {
+    currentPopup = WA.ui.openPopup("clockPopup", "Bienvenue dans la magie clinique", []);
+    console.log("Here welcome");
+})
 
-    WA.room.area.onLeave('test-area').subscribe(closePopup);
+WA.room.area.onLeave('test-area').subscribe(closePopup);
 
-function closePopup(){
+function closePopup() {
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
     }
 }
 
-export {};
+export { };
