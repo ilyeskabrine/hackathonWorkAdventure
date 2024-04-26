1b88e03b5db9729bb348a90b7ef248aae772feda
khelifa <akhelifa7@myges.fr>
2024-04-26 17:42:20 +0200

    Redirecting patients to the doctor's  room.

--- src/formulaire.ts
+++ src/formulaire.ts
@@ -12,15 +12,50 @@ const submitButton = document.getElementById("submitButton") as HTMLButtonElemen
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
-    submitButton.addEventListener("click", () => {
+    let rediractionPopup;
+    submitButton.addEventListener("click",  () => {
 
         WA.player.state.firstName = firstName.value;
         WA.player.state.lastName = lastName.value;
         WA.player.state.doctorName = doctorName.value;
         WA.player.state.message = message.value;
         WA.player.state.appointment = appointment.value;
+        console.log(doctorName.value);
+        if (doctorName.value == "Dr.Leroy") {
+            WA.player.moveTo(450, 240, 10);
+        } else if (doctorName.value == "Dr.Garcia") {
+            WA.player.moveTo(440, 300, 10);
+        } else if (doctorName.value == "Dr.Roux") {
+            WA.player.moveTo(870, 300, 10);
+        }else if (doctorName.value == "Dr.Fournier") {
+            WA.player.moveTo(700, 300, 10);
+        }else if (doctorName.value == "Dr.Lefebvre") {
+            WA.player.moveTo(720, 240, 10);
+        }
+
+
+
 
     });
+
+    WA.room.area.onEnter("getinfo").subscribe(() => {
+        rediractionPopup = WA.ui.openPopup("popupRectangle", 'Hello world!', [{
+            label: "Close",
+            className: "primary",
+            callback: (popup) => {
+                popup.close();
+            }
+
+        }]);
+        WA.player.moveTo(250, 250, 10);
+
+
+    });
+
+// Close the popup when we leave the zone.
+    WA.room.onLeaveLayer("myZone").subscribe(() => {
+        rediractionPopup.close();
+    })
 }).catch(e => console.error(e));
 
 bootstrapExtra().then(() => {
