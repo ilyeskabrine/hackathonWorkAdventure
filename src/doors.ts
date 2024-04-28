40e898b53f1150fb5d8639e3e6cc773f9dda48f9
fabian222222 <fabian.zuo@supinternet.fr>
2024-04-28 20:34:11 +0200

    new: add room handle depending on user role

--- src/doors.ts
+++ src/doors.ts
@@ -1,8 +1,14 @@
-import { NUMBER_OF_ROOM, DOOR_OPEN_NAME, DOOR_CLOSE_NAME, DOOR_ENTRY_ACTION, DOOR_POINT_NAME, DOOR_EXIT_ACTION } from "./constants";
+import { NUMBER_OF_ROOM, DOOR_OPEN_NAME, DOOR_CLOSE_NAME, DOOR_ENTRY_ACTION, DOOR_POINT_NAME, DOOR_EXIT_ACTION, JITSI_NAME } from "./constants";
 
 type DoorProps = {
     doorState: boolean
     nbrOfUsers: number
+}
+
+type DoctorProps = {
+    id: number
+    name: string
+    room: string
 }
 
 export const initAllDoors = () => {
@@ -23,7 +29,13 @@ export const onEntryDoorsPoint = async () => {
         WA.room.onEnterLayer(`floor_entry_exit/${DOOR_ENTRY_ACTION}${i}`).subscribe( async () => {
             const roomProperties = WA.state[`${DOOR_POINT_NAME}${i}`] as DoorProps
             if (roomProperties && roomProperties.nbrOfUsers < 2) {
-                WA.state[`${DOOR_POINT_NAME}${i}`] = {doorState: !roomProperties?.doorState, nbrOfUsers: roomProperties?.nbrOfUsers}
+                const isUserADoctor = checkIfUSerIsDoctor()
+                const isDoctorInRoom = checkIfRoomAlreadyContainDoctor(i)
+                const isPatientInRoom = checkIfRoomAlreadyContainPatient(i)
+
+                if ((isUserADoctor && !isDoctorInRoom) || (!isUserADoctor && !isPatientInRoom)) {
+                    WA.state[`${DOOR_POINT_NAME}${i}`] = {doorState: !roomProperties?.doorState, nbrOfUsers: roomProperties?.nbrOfUsers}
+                }
             }
         })
 
@@ -52,4 +64,28 @@ export const listenDoorsVariables = () => {
             }
         }); 
     }
+}
+
+export const checkIfUSerIsDoctor = () => {
+    const roomDoctors = WA.state['room-state']
+    const userId = WA.player.playerId
+    
+    const filterDoctorsWithPlayerId = roomDoctors.doctors.filter((doctor: DoctorProps) => {
+        if (doctor.id === userId) {
+            return doctor
+        }
+    })
+
+    return filterDoctorsWithPlayerId.length
+}
+
+export const checkIfRoomAlreadyContainDoctor = (roomIndex: number) => {
+    const room = WA.state['room-state'][`${JITSI_NAME}${roomIndex}`]
+    return room.doctor !== null
+}
+
+export const checkIfRoomAlreadyContainPatient = (roomIndex: number) => {
+    const room = WA.state['room-state'][`${JITSI_NAME}${roomIndex}`]
+    console.log(room, 'room')
+    return room.patient !== null
 }
